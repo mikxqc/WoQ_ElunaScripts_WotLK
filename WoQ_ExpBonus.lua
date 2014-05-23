b235904603ce915414ca1e8a68160ee0ce659f47
Bl00DGuY <bdsaTuv1>
2014-05-23 16:19:34 -0400

    Exp bonus adjustment

--- WoQ_ExpBonus.lua
+++ WoQ_ExpBonus.lua
@@ -22,31 +22,31 @@ local function pGainExp(event, player, amount, victim)
 	
 	if(pMaxLevel >= 1) then -- Check if the account has at least 1 level 80.
 		if (pMaxLevel == 1) then
-		local BonusPercent = amount / 4
+		local BonusPercent = amount
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 2) then
-		local BonusPercent = amount * 2 / 4
+		local BonusPercent = amount * 2
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 3) then
-		local BonusPercent = amount * 3 / 4
+		local BonusPercent = amount * 3
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 4) then
-		local BonusPercent = amount * 4 / 4
+		local BonusPercent = amount * 4
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 5) then
-		local BonusPercent = amount * 5 / 4
+		local BonusPercent = amount * 5
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 6) then
-		local BonusPercent = amount * 6 / 4
+		local BonusPercent = amount * 6
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 7) then
-		local BonusPercent = amount * 7 / 4
+		local BonusPercent = amount * 7
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 8) then
-		local BonusPercent = amount * 8 / 4
+		local BonusPercent = amount * 8
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		elseif (pMaxLevel == 9) then
-		local BonusPercent = amount * 9 / 4
+		local BonusPercent = amount * 9
 		player:GiveXP(BonusPercent, victim, false, 1.0)
 		else
 		end
